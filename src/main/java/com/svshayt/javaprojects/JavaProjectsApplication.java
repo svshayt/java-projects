7485c45a6a9253ceaa35fb3ddfe4434aa9e4fd87
Vasily Shaytan <svshayt@MacBook-Pro.local>
2023-08-12 23:40:36 +0300

    Initial commit

---
+++ src/main/java/com/svshayt/javaprojects/JavaProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.svshayt.javaprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svshayt/javaprojects/JavaProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svshayt.javaprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
